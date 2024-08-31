30abbf47d416029a58009eb023a0e38c259f32e3
leomoca19 <leomoca19@gmail.com>
2024-08-31 16:30:57 -0400

    add greeting and goodbye messages

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ def welcome():
 def goodbye():
     print('Goodbye!')
 
-# Press the green button in the gutter to run the script.
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    welcome()
+    goodbye()
