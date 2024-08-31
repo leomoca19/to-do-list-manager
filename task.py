aa21e08fa6592c76c06abf9562abc2a7ca97826b
leomoca19 <leomoca19@gmail.com>
2024-08-31 10:03:21 -0400

    Task object defined

---
+++ task.py
@@ -0,0 +1,8 @@
+class Task:
+    def __init__(self, id, description, status, date_created):
+        self.id = id
+        self.description = description
+        self.status = status
+        self.date_created = date_created
+
+
