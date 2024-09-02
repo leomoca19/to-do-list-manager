5fb56bf0486502d8d8a4f6f6178a3fa120bec15a
Leonardo_Caceres <leomoca19@gmail.com>
2024-09-02 12:06:09 -0400

    refactor

--- main.py
+++ main.py
@@ -46,97 +46,15 @@ def prompt(question, answers=None):
         pass
 
 
-def view_tasks(status=None):
-    """
-    Display all current tasks with an option to view completed and pending tasks separately.
-    """
-    print()
-    for task in tasks:
-        if task:
-            if status and status != task.status:
-                continue
-            task.display()
-    print()
-
-
-def add_task(task=None):
-    """
-    Adds a new tasks to the to-do list by prompting or argument
-    """
-
-    if task:
-        tasks.append(task)
-
-    else:
-        description = input() if not DEBUG else 'sample'
-        tasks.append(Task(description))
-
-
-def update_task(id):
-    """
-    Allow the user to mark tasks as complete and edit the task details
-    """
-
-    for task in tasks:
-        if task and task.id <= id:
-            print('Task selected: ')
-            task.display()
-            print()
-
-            repeat = True
-            while repeat:
-                print('Mark as completed? y|n: ', end='')
-
-                if answer := input().lower() in ['y', 'n']:
-                    repeat = False
-                    if answer == 'y':
-                        task.status = 'completed'
-
-                else:
-                    print('Bad input')
-
-            repeat = True
-            while repeat:
-                print('Update description? y|n: ', end='')
-
-                if input().lower() in ['y', 'n']:
-                    repeat = False
-                    print('New description: ', end='')
-                    task.description = input()
-
-                else:
-                    print('Bad input')
-
-            return
-
-
-def remove_task(id):
-    """
-    Deletes a task from the list by id
-    """
-    for i in range(len(tasks)):
-        if tasks[i].id == id:
-            tasks[i] = None
-            return
-
-
-def save_to_file():
-    pass
-
-
-def load_from_file():
-    pass
-
-
 if __name__ == '__main__':
     welcome()
 
     for _ in range(3):
-        add_task()
+        tm.add_task()
 
-    view_tasks()
-    remove_task(2)
-    update_task(3)
-    view_tasks()
+    tm.view_tasks()
+    tm.remove_task(2)
+    tm.update_task(3)
+    tm.view_tasks()
 
     goodbye()
