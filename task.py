02d344322bd5b35a38cf89cb4068e36a6d8686e1
leomoca19 <leomoca19@gmail.com>
2024-09-03 16:18:26 -0400

    remove id

--- task.py
+++ task.py
@@ -10,7 +10,6 @@ tasks = []
 
 class Task:
     def __init__(self, description):
-        self.id = len(tasks) + 1
         self.description = description
         self.status = 'pending'
         self.date = datetime.now().date().strftime("%b-%d-%Y")
@@ -52,38 +51,34 @@ def update_task(id):
     """
     Allow the user to mark tasks as complete and edit the task details
     """
+    if task := tasks[id]:
+        print('Task selected: ')
+        task.display()
+        print()
 
-    for task in tasks:
-        if task and task.id <= id:
-            print('Task selected: ')
-            task.display()
-            print()
+        repeat = True
+        while repeat:
+            print('Mark as completed? y|n: ', end='')
 
-            repeat = True
-            while repeat:
-                print('Mark as completed? y|n: ', end='')
+            if answer := input().lower() in ['y', 'n']:
+                repeat = False
+                if answer == 'y':
+                    task.status = 'completed'
 
-                if answer := input().lower() in ['y', 'n']:
-                    repeat = False
-                    if answer == 'y':
-                        task.status = 'completed'
+            else:
+                print('Bad input')
 
-                else:
-                    print('Bad input')
+        repeat = True
+        while repeat:
+            print('Update description? y|n: ', end='')
 
-            repeat = True
-            while repeat:
-                print('Update description? y|n: ', end='')
+            if input().lower() in ['y', 'n']:
+                repeat = False
+                print('New description: ', end='')
+                task.description = input()
 
-                if input().lower() in ['y', 'n']:
-                    repeat = False
-                    print('New description: ', end='')
-                    task.description = input()
-
-                else:
-                    print('Bad input')
-
-            return
+            else:
+                print('Bad input')
 
 
 def remove_task(id):
