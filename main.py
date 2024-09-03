160a8659af4f2802f8d12c0a3adbfd062d51157f
Leonardo_Caceres <leomoca19@gmail.com>
2024-09-03 16:55:01 -0400

    move debug to main.py

--- main.py
+++ main.py
@@ -3,15 +3,17 @@ Simple command-line application to manage tasks, including adding, updating, del
 The tasks will be stored in a local file so that the user’s to-do list persists between sessions.
 """
 
-from task import add_task, view_tasks, update_task, remove_task
+from task import Task, add_task, view_tasks, update_task, remove_task
 from utilities import welcome, goodbye
 
+DEBUG = True
 
 if __name__ == '__main__':
     welcome()
+    t = Task('sample') if DEBUG else None
 
     for _ in range(3):
-        add_task()
+        add_task(t)
 
     view_tasks()
     remove_task(2)
